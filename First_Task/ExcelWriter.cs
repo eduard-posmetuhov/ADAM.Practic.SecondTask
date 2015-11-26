cebb81cfe3c2d484f270e695f50af48af5222b7a
eduard-posmetuhov <eduard.posmetuhov@gmail.com>
2015-11-26 11:20:13 +0300

    Second Task

---
+++ First_Task/ExcelWriter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using Microsoft.Office.Core;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace First_Task
+{
+    class ExcelWriter
+    {
+        //private Dictionary<Guid, string> _source;
+        private readonly string _fileName;
+
+        public ExcelWriter(string Path)
+        {
+            _fileName=Path;
+        }
+
+        public bool Write(Dictionary<Guid,string> source)
+        {
+            Excel.Application myExcelApp;
+            Excel.Workbook myExcelWorkbook;
+            Excel.Worksheet myExcelWorksheet;
+            myExcelApp = new Excel.Application();
+            object misValue = System.Reflection.Missing.Value;
+            myExcelWorkbook = myExcelApp.Workbooks.Add(misValue);
+            myExcelWorksheet = (Excel.Worksheet)myExcelWorkbook.Worksheets.Item[1]; 
+                        
+            myExcelWorksheet.get_Range("A1", misValue).Formula = "UPC code";
+            myExcelWorksheet.get_Range("B1", misValue).Formula = "Link";
+            int row = 2;
+            foreach(KeyValuePair<Guid,string> pair in source)
+            {               
+                myExcelWorksheet.Cells[row,1] = pair.Value;
+                myExcelWorksheet.Cells[row,2]= @"https://evbyminsd38fe/Assets/Records/~"+pair.Key;
+                row++;
+            }
+
+            myExcelWorkbook.SaveAs(_fileName, Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue,
+                Excel.XlSaveAsAccessMode.xlExclusive,
+                misValue, misValue, misValue, misValue, misValue);
+
+            myExcelWorkbook.Close(true, misValue, misValue);
+            myExcelApp.Quit();
+            return true;
+        }
+    }
+}
---
+++ First_Task/Program.cs
@@ -0,0 +1,71 @@
+﻿using Adam.Core;
+using Adam.Core.Classifications;
+using Adam.Core.Fields;
+using Adam.Core.Records;
+using Adam.Core.Search;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Application app = new Application();
+            LogIn(app);            
+
+            Classification c = new Classification(app);
+            ClassificationHelper ch = new ClassificationHelper(app);
+            Guid? rootGuid= ch.GetId(new SearchExpression("name = 'Luxottica Content*'"));            
+            
+            RecordCollection rc = new RecordCollection(app);
+            rc.Load(new SearchExpression(String.Format("classification = '{0}'", rootGuid)));
+            Dictionary<string, Guid> dictionaryForSearch = new Dictionary<string, Guid>();
+            Dictionary<Guid, string> dictionaryForExcel = new Dictionary<Guid, string>();            
+            string currentValue=null;
+            Console.WriteLine(rc.Count);
+            foreach (Record r in rc.Take<Record>(100))
+            {                
+                if (r.Fields.GetField<TextField>("UPC code") != null)
+                {
+                    if (!String.IsNullOrEmpty(r.Fields.GetField<TextField>("UPC code").Value))
+                    {
+                        currentValue = r.Fields.GetField<TextField>("UPC code").Value.TrimStart('0');
+                        if(dictionaryForSearch.ContainsKey(currentValue)/* .Contains(currentValue)*/)
+                        {
+                            dictionaryForExcel.Add(r.Id,currentValue);
+                            Guid g = dictionaryForSearch[currentValue];
+                            if (!dictionaryForExcel.ContainsKey(g))
+                            {
+                                dictionaryForExcel.Add(g, currentValue);
+                            }
+                        }
+                        else dictionaryForSearch.Add(currentValue,r.Id);
+                        Console.WriteLine(currentValue);
+                    }
+                }
+            }
+            Console.WriteLine(dictionaryForExcel.Count);
+            ExcelWriter writer = new ExcelWriter(@"D:\book1.xls");
+            writer.Write(dictionaryForExcel);
+            Console.ReadKey();            
+        }
+
+        private static void LogIn(Application app)
+        {
+            LogOnStatus status = app.LogOn("LUXDAM", "Eduard_Pasmetukhau", "P2ssw0rd!");
+            if (status == LogOnStatus.LoggedOn)
+            {
+                Console.WriteLine("Ok");
+            }
+            else Console.WriteLine(status);
+
+        }
+    }
+
+    
+}
