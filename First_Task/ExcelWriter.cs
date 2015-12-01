3d646b91081b3a63ba82d0123527031a4d79c092
eduard-posmetuhov <eduard.posmetuhov@gmail.com>
2015-12-01 14:49:14 +0300

    Update with settings

--- First_Task/ExcelWriter.cs
+++ First_Task/ExcelWriter.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Adam.Core.Records;
+using System;
 using System.Collections.Generic;
+using Adam.Core.Fields;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +12,6 @@ namespace First_Task
 {
     class ExcelWriter
     {
-        //private Dictionary<Guid, string> _source;
         private readonly string _fileName;
 
         public ExcelWriter(string Path)
@@ -18,32 +19,39 @@ namespace First_Task
             _fileName=Path;
         }
 
-        public bool Write(Dictionary<Guid,string> source)
+        public bool Write(Dictionary<Guid,string> source, Adam.Core.Application app)
         {
-            Excel.Application myExcelApp;
-            Excel.Workbook myExcelWorkbook;
-            Excel.Worksheet myExcelWorksheet;
-            myExcelApp = new Excel.Application();
+            Record r = new Record(app);
+            Excel.Application EApp;
+            Excel.Workbook EWorkbook;
+            Excel.Worksheet EWorksheet;
+            Excel.Range Rng;
+            EApp = new Excel.Application();
             object misValue = System.Reflection.Missing.Value;
-            myExcelWorkbook = myExcelApp.Workbooks.Add(misValue);
-            myExcelWorksheet = (Excel.Worksheet)myExcelWorkbook.Worksheets.Item[1]; 
-                        
-            myExcelWorksheet.get_Range("A1", misValue).Formula = "UPC code";
-            myExcelWorksheet.get_Range("B1", misValue).Formula = "Link";
+            EWorkbook = EApp.Workbooks.Add(misValue);
+            EWorksheet = (Excel.Worksheet)EWorkbook.Worksheets.Item[1];                         
+            EWorksheet.get_Range("A1", misValue).Formula = "UPC code";
+            EWorksheet.get_Range("B1", misValue).Formula = "Link";            
+            Rng = EWorksheet.get_Range("A2", misValue).get_Resize(source.Count,misValue);
+            Rng.NumberFormat = "00000000000000";
             int row = 2;
             foreach(KeyValuePair<Guid,string> pair in source)
-            {               
-                myExcelWorksheet.Cells[row,1] = pair.Value;
-                myExcelWorksheet.Cells[row,2]= @"https://evbyminsd38fe/Assets/Records/~"+pair.Key;
+            {              
+                EWorksheet.Cells[row,1] = pair.Value;                
+                r.Load(pair.Key);
+                Rng = EWorksheet.get_Range("B"+row, misValue);
+                EWorksheet.Hyperlinks.Add(Rng, r.Fields.GetField<TextField>("Content Url").Value);               
+                //myExcelWorksheet.Cells[row, 2] = r.Fields.GetField<TextField>("Content Url").Value;                                
                 row++;
             }
-
-            myExcelWorkbook.SaveAs(_fileName, Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue,
+            ((Excel.Range)EWorksheet.Cells[2, 1]).EntireColumn.AutoFit();
+            ((Excel.Range)EWorksheet.Cells[2, 2]).EntireColumn.AutoFit();
+            EWorkbook.SaveAs(_fileName, Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue,
                 Excel.XlSaveAsAccessMode.xlExclusive,
                 misValue, misValue, misValue, misValue, misValue);
 
-            myExcelWorkbook.Close(true, misValue, misValue);
-            myExcelApp.Quit();
+            EWorkbook.Close(true, misValue, misValue);
+            EApp.Quit();
             return true;
         }
     }
--- First_Task/Program.cs
+++ First_Task/Program.cs
@@ -6,6 +6,7 @@ using Adam.Core.Search;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Configuration;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,54 +14,56 @@ namespace First_Task
 {
     class Program
     {
+        private static Application _app;
         static void Main(string[] args)
-        {            
-            Application app = new Application();
-            LogIn(app);            
-
-            Classification c = new Classification(app);
-            ClassificationHelper ch = new ClassificationHelper(app);
-            Guid? rootGuid= ch.GetId(new SearchExpression("name = 'Luxottica Content*'"));            
-            
-            RecordCollection rc = new RecordCollection(app);
-            rc.Load(new SearchExpression(String.Format("classification = '{0}'", rootGuid)));
-            Dictionary<string, Guid> dictionaryForSearch = new Dictionary<string, Guid>();
-            Dictionary<Guid, string> dictionaryForExcel = new Dictionary<Guid, string>();            
-            string currentValue=null;
-            Console.WriteLine(rc.Count);
-            foreach (Record r in rc.Take<Record>(100))
-            {                
-                if (r.Fields.GetField<TextField>("UPC code") != null)
+        {
+            try
+            {
+                _app = new Application();
+                LogIn(_app);
+                RecordCollection rc = new RecordCollection(_app);
+                rc.Load(new SearchExpression("FieldName('UPC code') = *"));
+                Dictionary<string, Guid> dictionaryForSearch = new Dictionary<string, Guid>();
+                Dictionary<Guid, string> dictionaryForExcel = new Dictionary<Guid, string>();
+                string currentValue = null;               
+                foreach (Record r in rc)
                 {
-                    if (!String.IsNullOrEmpty(r.Fields.GetField<TextField>("UPC code").Value))
-                    {
-                        currentValue = r.Fields.GetField<TextField>("UPC code").Value.TrimStart('0');
-                        if(dictionaryForSearch.ContainsKey(currentValue)/* .Contains(currentValue)*/)
+                    currentValue = r.Fields.GetField<TextField>("UPC code").Value.TrimStart('0');
+                    if (!String.IsNullOrEmpty(currentValue))
+                    {                        
+                        if (dictionaryForSearch.ContainsKey(currentValue))
                         {
-                            dictionaryForExcel.Add(r.Id,currentValue);
+                            dictionaryForExcel.Add(r.Id, currentValue);
                             Guid g = dictionaryForSearch[currentValue];
                             if (!dictionaryForExcel.ContainsKey(g))
-                            {
-                                dictionaryForExcel.Add(g, currentValue);
-                            }
+                                dictionaryForExcel.Add(g, currentValue); 
                         }
-                        else dictionaryForSearch.Add(currentValue,r.Id);
-                        Console.WriteLine(currentValue);
+                        else
+                            dictionaryForSearch.Add(currentValue, r.Id);                        
                     }
                 }
-            }
-            Console.WriteLine(dictionaryForExcel.Count);
-            ExcelWriter writer = new ExcelWriter(@"D:\book1.xls");
-            writer.Write(dictionaryForExcel);
-            Console.ReadKey();            
+                Console.WriteLine("Count of duplicate assets: " + dictionaryForExcel.Count);
+                ExcelWriter writer = new ExcelWriter(ConfigurationSettings.AppSettings["ExcelPath"]);
+                writer.Write(dictionaryForExcel,_app);
+                Console.WriteLine("Save complite");
+                Console.ReadKey();
+            }            
+            finally
+            {
+                if(_app.IsLoggedOn)
+                _app.LogOff();
+            }            
         }
 
         private static void LogIn(Application app)
         {
-            LogOnStatus status = app.LogOn("LUXDAM", "Eduard_Pasmetukhau", "P2ssw0rd!");
+            string connection = ConfigurationSettings.AppSettings["AdamRegistrationName"];
+            string user = ConfigurationSettings.AppSettings["AdamAdminUserName"];
+            string pass = ConfigurationSettings.AppSettings["AdamAdminPassword"];
+            LogOnStatus status = app.LogOn(connection, user, pass);
             if (status == LogOnStatus.LoggedOn)
             {
-                Console.WriteLine("Ok");
+                Console.WriteLine("LogOn complite");
             }
             else Console.WriteLine(status);
 
